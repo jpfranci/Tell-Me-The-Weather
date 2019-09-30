75d87de3951562bb47ec4c3a712df6ede8350203
JP <jpbfrancisco1998@gmail.com>
2019-09-29 21:06:28 -0700

    changed some messages

--- WeatherScript.py
+++ WeatherScript.py
@@ -4,22 +4,21 @@ from ApiKeys import twilioKeys, darkSky, numbersToText, latitude, longitude
 import requests
 
 TEMPERATURE_MESSAGES = {
-    "freezing": "Oh no :( It's gonna be really cold today: {0} degrees at {1}!!\nThe high today is {2} degrees at {3}.\nMake sure you really layer up and wear a good winter coat!\n\n",
+    "freezing": "It's gonna be really cold today: {0} degrees at {1}!!\nThe high today is {2} degrees at {3}.\nMake sure you layer up and wear a good jacket.\n\n",
     "sweaterWeather": "It's gonna get chilly today with a low of: {0} degrees at {1}.\nThe high today is {2} degrees at {3}.\nMake sure you wear a sweater!\n\n",
-    "sweaterLayers": "It's gonna get chilly today: {0} degrees at {1}!\nBut it will get hotter to {2} degrees at {3}, so make sure you wear something you can take off\n\n",
+    "sweaterLayers": "It's gonna get chilly today: {0} degrees at {1}!\nBut it will get hotter to {2} degrees at {3}, so make sure you wear something you can take off when it gets warmer\n\n",
     "perfectWeather": "We're gonna have great weather today: {0} degrees at {1} :)\nThe low is {2} degrees at {3}.\nYou can layer lightly if you want\n\n ",
     "warmWeather": "It's gonna be warm today! It's gonna get up to {0} degrees at {1}!\nThe low is {2} degrees at {3}.\nMake sure you wear something light.\n\n",
     "hotWeather": "It's gonna be really hot today. It's gonna get up to {0} degrees at {1}!.\nThe low is {2} degrees at {3}.\nnDon't wear anything heavy today!!\n\n"
 }
 PRECIPITATION_MESSAGES = {
-    "precipitation": "Oh no! There is a {0}% chance of {1} today\n",
-    "noPrecipitation": "Yay! You don't have to worry about rain today because there is only a {0}% chance of {1} today!\n"
+    "precipitation": "There is a {0}% chance of {1} today\n",
+    "noPrecipitation": "You don't have to worry too much about rain today because there is only a {0}% chance of {1} today.\n"
 }
 AVERAGE_TEMP_MESSAGES = {
     "averageTemp": "Average temperature from {0} to {1}: {2} degrees\n"
 }
 PRECIPITATION_MESSAGE_THRESHOLD = 20
-HOURS_OF_SUN_THRESHOLD = 2
 
 def getOutfitSuggestion(minTemp, maxTemp):
     message = ""
@@ -41,12 +40,6 @@ def getOutfitSuggestion(minTemp, maxTemp):
             message = TEMPERATURE_MESSAGES["perfectWeather"]
         message = message.format(maxTemp["temp"], maxTemp["time"], minTemp["temp"], minTemp["time"])
     return message
-        
-def getHoursOfSunMessage(hoursOfSun):
-    if hoursOfSun > HOURS_OF_SUN_THRESHOLD:
-        return "Amazing, we have {0} hrs of sun today!\n".format(hoursOfSun)
-    else:
-        return ""
 
 def getPrecipitationMessage(precipitationProb, precipitationType):
     message = ""
@@ -56,7 +49,7 @@ def getPrecipitationMessage(precipitationProb, precipitationType):
 def addTimePrecipitationMessage(message, precipitationType, precipitationProbability):
     if precipitationProbability >= PRECIPITATION_MESSAGE_THRESHOLD:
         message += PRECIPITATION_MESSAGES["precipitation"].format(precipitationProbability, precipitationType)
-        message += "You should probably bring an umbrella :( \n\n"
+        message += "You should probably bring an umbrella. \n\n"
     else: 
         message += PRECIPITATION_MESSAGES["noPrecipitation"].format(precipitationProbability, precipitationType)
     return message
@@ -78,14 +71,13 @@ def getTimeFromSeconds(seconds):
 
 def getAverageTemperatures(weatherArray):
     averageTemp = sum(list(map(lambda hourlyData : hourlyData["apparentTemperature"], weatherArray))) / len(weatherArray)
-    averageTemp = round(averageTemp)
     return {
-        "temp": averageTemp,
+        "temp": round(averageTemp),
         "startTime": getTimeFromSeconds(weatherArray[0]["time"]),
         "endTime": getTimeFromSeconds(weatherArray[-1]["time"])
     }
 
-def getAverageTemperaturesForMorningToEvening(hourlyData):
+def getAverageTempsThrougDay(hourlyData):
     averageTemps = {}
     averageTemps["morning"] = getAverageTemperatures(hourlyData[0:6])
     averageTemps["afternoon"] = getAverageTemperatures(hourlyData[6:12])
@@ -108,9 +100,8 @@ def getMessagesForWeather(weatherJson):
         "precipitationProb": round(weatherData['precipProbability'] * 100),
         "precipType": weatherData.get('precipType', 'precipitation'),
         "alerts": weatherData.get("alerts", []),
-        "averageTempsThroughDay": getAverageTemperaturesForMorningToEvening(hourlyData)
+        "averageTempsThroughDay": getAverageTempsThrougDay(hourlyData)
     }
-    print(weatherInfo)
     message += getOutfitSuggestion(weatherInfo["minTemp"], weatherInfo["maxTemp"])
     message += getAverageTempMessages(weatherInfo["averageTempsThroughDay"])
     message += getPrecipitationMessage(weatherInfo["precipitationProb"], weatherInfo["precipType"]) 
@@ -120,6 +111,7 @@ def getMessagesForWeather(weatherJson):
 def textWeather(weatherJson):
     client = Client(twilioKeys["accountSid"], twilioKeys["authToken"])
     messageToText = "\n\n" + getMessagesForWeather(weatherJson)
+    
     for number in numbersToText:
         client.messages.create(
             to= number,
